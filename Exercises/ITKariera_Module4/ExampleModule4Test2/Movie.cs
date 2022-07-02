6bb04fcb13b6c4ccfc0f124f5a2112efcd424373
Nsgshsk <100464409+Nsgshsk@users.noreply.github.com>
2022-07-02 23:39:29 +0300

    Added another exercise before the exam

---
+++ Exercises/ITKariera_Module4/ExampleModule4Test2/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleModule4Test2
+{
+    public class Movie
+    {
+        private string title;
+        private double rating;
+
+        public string Title { get { return title; } }
+        public double Rating { get { return rating; } }
+
+        public Movie(string title, double rating)
+        {
+            this.title = title;
+            this.rating = rating;
+        }
+        public override string ToString()
+        {
+            return $"Movie {title} is with {rating:F1} rating.";
+        }
+    }
+}
---
+++ Exercises/ITKariera_Module4/ExampleModule4Test2/VideoLibrary.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleModule4Test2
+{
+    public class VideoLibrary
+    {
+        private string name;
+        private List<Movie> movies;
+
+        public string Name { get { return name; } }
+        public List<Movie> Movies { get { return movies; } }
+
+        public VideoLibrary(string name)
+        {
+            this.name = name;
+            movies = new List<Movie>();
+        }
+        public void AddMovie(string title, double rating)
+        {
+            movies.Add(new Movie(title, rating));
+        }
+        public double AverageRating()
+        {
+            //return movies.Average(e => e.Rating);
+            double avg = 0; 
+            foreach (var item in movies)
+            {
+                avg += item.Rating;
+            }
+            return Math.Round(avg / movies.Count, 1);
+        }
+        public List<string> GetMoviesByRating(double rating)
+        {
+            List<string> list = new List<string>();
+            foreach (var item in movies)
+            {
+                if (item.Rating > rating)
+                {
+                    list.Add(item.Title);
+                }
+            }
+            return list;
+        }
+        public List<Movie> SortByTitle()
+        {
+            //movies.OrderBy(e => e.Title);
+            bool swap;
+            do
+            {
+                swap = false;
+                for (int i = 0; i < movies.Count - 1; i++)
+                {
+                    if (movies[i].Title[0] > movies[i + 1].Title[0])
+                    {
+                        var tmp = movies[i];
+                        movies[i] = movies[i + 1];
+                        movies[i + 1] = tmp;
+                        swap = true;
+                    }
+                }
+            } while (swap);
+            return movies;
+        }
+        public List<Movie> SortByRating()
+        {
+            //movies.OrderByDecending(e => e.Rating);
+            bool swap;
+            do
+            {
+                swap = false;
+                for (int i = 0; i < movies.Count - 1; i++)
+                {
+                    if (movies[i].Rating < movies[i + 1].Rating)
+                    {
+                        var tmp = movies[i];
+                        movies[i] = movies[i + 1];
+                        movies[i + 1] = tmp;
+                        swap = true;
+                    }
+                }
+            } while (swap);
+            return movies;
+        }
+        public string[] ProvideInformationAboutAllMovies()
+        {
+            string[] list = new string[movies.Count];
+            for (int i = 0; i < movies.Count; i++)
+            {
+                list[i] = movies[i].ToString();
+            }
+            return list;
+        }
+        public bool CheckMovieIsInVideoLibrary(string title)
+        {
+            foreach (var item in movies)
+            {
+                if (item.Title == title) return true;
+            }
+            return false;
+        }
+    }
+}
